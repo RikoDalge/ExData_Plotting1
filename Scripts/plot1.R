a3fd9eda2c8f9f7f12af7a7873e252636f07a87a
RikoDalge <ricodalge@gmail.com>
2015-05-10 09:44:11 -0300

    Update plot1.R

--- Scripts/plot1.R
+++ Scripts/plot1.R
@@ -1,6 +1,15 @@
-source("./get_clean_data.R")
+#Plot 1
 
-days <- get_data()
+#Check to see if the data is already in.
+if (!exists("days", inherits = FALSE)){  
+        source("./get_clean_data.R")
+        days <- get_data()
+}
+
+
+if(!file.exists("plots")){
+        dir.create("plots")
+}
 
 png(filename='./plots/plot1.png',width=480,height=480,units='px')
 
